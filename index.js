7b79a35c812e905bfc31d8fbb5c593ea43622660
stefanus dody <stefanusdody@yahoo.com>
2018-04-12 14:38:33 +0700

    hellotodo api

---
+++ index.js
@@ -0,0 +1,68 @@
+const cors = require("cors")
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+// body-Parser umumnya digunakan dalam form yang membutuhkan banyak object
+// params umumny digunakan dalam id
+
+// var requestTime = function(req, res, next) {
+//   req.requestTime = new Date();
+//   next();
+// };
+
+let todoList = [
+  { todo: "learn NodeJs", done: false },
+  { todo: "Learn ReactJs", done: false }
+];
+// app.use(requestTime);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+app.get("/todo", (req, res) => {
+  res.send({ data: todoList });
+});
+
+app.get("/todo/:id", (req, res) => {
+  let length = todoList.length;
+  let index = req.params.id;
+
+  if (index > length - 1) {
+    res.send("not found");
+  } else {
+    res.send({ data: todoList[index] });
+    console.log(index, length);
+  }
+});
+
+app.post("/todo", (req, res) => {
+  let todo = req.body.todo;
+  let done = JSON.parse(req.body.done);
+  console.log(typeof done);
+  if (todo === "") {
+    res.send("todo cannont empty");
+  } else {
+    let newTodo = {
+      todo: req.body.todo,
+      done: done
+    };
+    todoList.push(newTodo);
+    res.send({ sucess: true, data: newTodo });
+  }
+});
+
+app.delete("/todo/:id", (req, res) => {
+  let length = todoList.length;
+  let index = req.params.id;
+
+  if (index > length - 1) {
+    res.send({ sucess: false, message: "data not found" });
+  } else {
+    todoList.splice(index, 1);
+    res.send({ success: true, data: todoList });
+  }
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
